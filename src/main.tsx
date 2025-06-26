1ea269ec3f3d2af15fdce0f5e280580242edcf95
seon022 <seon022@icloud.com>
2025-06-26 23:33:13 +0900

    chore: develop 최신화 후 lint:fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './App.tsx';
-import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './theme.ts';
 
 const queryClient = new QueryClient();
@@ -12,7 +12,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <CssBaseline/>
+      <CssBaseline />
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
