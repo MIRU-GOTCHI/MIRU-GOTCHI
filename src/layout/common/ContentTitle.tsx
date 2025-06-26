8cb1b352e03f18a09b8eb1e832903a7fffbf3035
Seam Park <run128@naver.com>
2025-06-26 17:28:58 +0900

    feat: them 설정 및 공통 컴포넌트 추가

--- src/layout/common/ContentTitle.tsx
+++ src/layout/common/ContentTitle.tsx
@@ -5,13 +5,16 @@ interface ContentTitleProps {
 }
 
 const ContentTitleBar = styled("div") ({
-  margin: "0 -20px",
+  margin: "0 -20px 10px",
   display: "flex",
   alignItems: "center",
   padding: "0 20px",
   minHeight: "40px",
   "& > button": {
     marginLeft: "-20px",
+  },
+  "& h2, & h3":{
+    fontSize: "16px"
   }
 });
 
